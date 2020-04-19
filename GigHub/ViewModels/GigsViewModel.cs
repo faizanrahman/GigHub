0e7e2b81ff30092bb09922436240a168d0f850cb
faizanrahman <faizanrahman89@gmail.com>
2020-04-19 14:24:26 -0500

    Implement "Attendances" and "Following" use cases, create shared View

---
+++ GigHub/ViewModels/GigsViewModel.cs
@@ -0,0 +1,16 @@
+﻿using GigHub.Models;
+using System.Collections.Generic;
+
+namespace GigHub.ViewModels
+{
+	public class GigsViewModel
+	{
+		public GigsViewModel()
+		{
+		}
+
+		public IEnumerable<Gig> UpcomingGigs { get; set; }
+		public bool ShowActions { get; set; }
+		public string Heading { get; set; }
+	}
+}
